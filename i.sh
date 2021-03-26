319493e2b63e3f66078d78d42b2fd859ba06a09e
ars7v <81354008+ars7v@users.noreply.github.com>
2021-03-26 08:56:01 +0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -28,3 +28,5 @@ rm -f ngrok-stable-linux-amd64.zip & wait
 sudo chmod +x ngrok & wait
 sudo mv ngrok /usr/local/bin/ & wait
 
+sudo /usr/local/bin/ngrok authtoken 1qC4wDG4OxkTkDZPgmNAJAkmH7N_61a9nCVvokqwgTgv5eBZi
+
