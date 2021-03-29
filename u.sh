845d1cfeb3ec830a0947f415917d7e505b987001
ars7v <81354008+ars7v@users.noreply.github.com>
2021-03-29 09:23:09 +0300

    Update u.sh

--- u.sh
+++ u.sh
@@ -2,6 +2,18 @@
 # u.sh
 
 
+rm -f /usr/local/bin/ngrok
+
+wget -O ngrok-stable-linux-amd64.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip & wait
+
+unzip ngrok-stable-linux-amd64.zip ngrok & wait
+rm -f ngrok-stable-linux-amd64.zip & wait
+sudo chmod +x ngrok & wait
+sudo mv ngrok /usr/local/bin/ & wait
+
+sudo /usr/local/bin/ngrok authtoken 1qC4wDG4OxkTkDZPgmNAJAkmH7N_61a9nCVvokqwgTgv5eBZi
+
+
 rm -f /tmp/test0 & wait
 echo "$(whoami)" > /tmp/test0 & wait
 
